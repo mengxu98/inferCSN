a2bccd73bf3b1364e2063647e117b105ac5fe813
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-03-02 21:04:24 +0800

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' @docType data
+#' @name exampleDataMatrix
+#'
+#' @title Test data
+#' @description A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#' @details Success!!
+#'
+#' @format A matrix with 5000 rows and 18 variables:
+#' \describe{
+#'   \item{matrix}{The matrix used for reconstruct gene regulatory network}
+#'   ...
+#' }
+#' @source \url{NULL}
+NULL
