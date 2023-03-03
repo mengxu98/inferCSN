68b589be6e5a900aa860c4fa5ceea0d50c290318
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-03-03 16:24:35 +0800

    Update data.R

--- R/data.R
+++ R/data.R
@@ -2,7 +2,23 @@
 #' @name exampleDataMatrix
 #'
 #' @title Test data
-#' @description A dataset containing the prices and other attributes of almost 54,000
+#' @description A dataset
+#' diamonds.
+#' @details Success!!
+#'
+#' @format A matrix with 5000 rows and 18 variables:
+#' \describe{
+#'   \item{matrix}{The matrix used for reconstruct gene regulatory network}
+#'   ...
+#' }
+#' @source \url{NULL}
+NULL
+
+#' @docType data
+#' @name exampleDataGroundTruth
+#'
+#' @title Test data
+#' @description A dataset
 #' diamonds.
 #' @details Success!!
 #'
