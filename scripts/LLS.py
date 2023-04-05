2bdf78110d6310805d93e5c434bb5391b61726ef
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-04-05 15:21:01 +0800

    Update LLS.py

--- scripts/LLS.py
+++ scripts/LLS.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-#take corr network and genespace as input and create conditional LLS benchmarking data .condLLS
-#Author : Junha Cha
-#Date : Apr 24th 2021
-#usage : ./script.py [edgelist] [genelist.txt] 
-#outfile format : [bin_link] [cumLLS] [TP] [FP] [Gene_Coverage] [binLLS]
+# take corr network and genespace as input and create conditional LLS benchmarking data .condLLS
+# Author : Junha Cha
+# Date : Apr 24th 2021
+# usage : ./script.py [edgelist] [genelist.txt]
+# outfile format : [bin_link] [cumLLS] [TP] [FP] [Gene_Coverage] [binLLS]
 
 import sys
 import pandas as pd
 import numpy as np
-from math import * 
+from math import *
 
-#print(sys.version)
-#exit()
+# print(sys.version)
+# exit()
 
 '''
 def calc_pearsonr_fast(expr_data, absort = True):
@@ -39,52 +39,55 @@ def calc_pearsonr_fast(expr_data, absort = True):
     return edges_all
 '''
 
+
 def read_edgelist(file_path):
     net = pd.read_csv(file_path, sep='\t')
     net.columns = ['gene1', 'gene2', 'PCC']
 
-    #join two links as tuple
+    # join two links as tuple
     cols = ['gene1', 'gene2']
     net['genepair'] = list(zip(net.gene1, net.gene2))
 
-    #rename network rowname
+    # rename network rowname
     net.index = net.genepair
-    edges_all = net.loc[:, 'PCC'] #series
+    edges_all = net.loc[:, 'PCC']  # series
 
     return edges_all
+
 
 def load_gold_standard_pairs(gsfile):
     network = dict()
     with open(gsfile) as GS:
         for line in GS:
             a, b = line.strip().split("\t")
-            #take gs only in the input genelist
-            #if a in genelist and b in genelist: 
+            # take gs only in the input genelist
+            # if a in genelist and b in genelist:
             if a not in network:
                 network[a] = dict()
             if b not in network:
                 network[b] = dict()
             network[a][b] = True
             network[b][a] = True
-            
+
     return network
+
 
 def run_benchmark_LLS(target_network,
                       gold_standard_pairs,
-                      already_sorted = True,
-                      ascending = False,
-                      max_pair = 1000000,
+                      already_sorted=True,
+                      ascending=False,
+                      max_pair=1000000,
                       binsize=1000):
 
-    # calculate positive, negative pairs
+    # Calculate positive, negative pairs
     pos = 0
     for g in gold_standard_pairs:
-        pos+=len(gold_standard_pairs[g])
-    
-    pos /= 2
-    neg = ( len(gold_standard_pairs) * (len(gold_standard_pairs)-1) / 2 ) - pos
+        pos += len(gold_standard_pairs[g])
 
-    # calculate pr curve
+    pos /= 2
+    neg = (len(gold_standard_pairs) * (len(gold_standard_pairs)-1) / 2) - pos
+
+    # Calculate pr curve
     TP = 0
     FP = 0
     FN = pos
@@ -93,45 +96,48 @@ def run_benchmark_LLS(target_network,
 
     count = 0
     print('prior(GS) ratio')
-    print("%f"%(float(pos)/float(neg)))
+    print("%f" % (float(pos)/float(neg)))
 
     randomprecision = float(pos)/float(pos+neg)
     priorprob = float(pos)/float(neg)
-    #print(priorprob)
+    # print(priorprob)
     print('Prior PR')
     print(randomprecision)
     already = set()
     benchdata = dict()
-    binstats=list()
+    binstats = list()
 
     if already_sorted == False:
-        sorted_keys = sorted(target_network,key=target_network.get,reverse = not ascending)
+        sorted_keys = sorted(
+            target_network,
+            key=target_network.get,
+            reverse=not ascending)
     else:
         sorted_keys = target_network.keys()
 
     for pair in sorted_keys:
-        if count>=max_pair:
+        if count >= max_pair:
             break
 
         already.add(pair[0])
         already.add(pair[1])
 
         binstats.append(target_network[pair])
-        count+=1
-        
+        count += 1
+
         if pair[0] in gold_standard_pairs and pair[1] in gold_standard_pairs[pair[0]]:
             TP += 1
             FN -= 1
-            binTP+= 1
+            binTP += 1
         else:
             FP += 1
             binFP += 1
 
-        if count%binsize == 0:
+        if count % binsize == 0:
             precision = float(TP)/float(TP+FP)
             recall = float(TP)/float(TP+FN)
             oddratio = precision/randomprecision
-            if FP!=0:
+            if FP != 0:
                 try:
                     lls = log((float(TP)/float(FP)) / priorprob) / log(2)
                 except ValueError:
@@ -140,44 +146,50 @@ def run_benchmark_LLS(target_network,
                     exit()
             else:
                 lls = 20.0
-            if binTP!=0:
-                if binFP!=0:
-                    binlls = log((float(binTP)/float(binFP)) / priorprob) / log(2)
+            if binTP != 0:
+                if binFP != 0:
+                    binlls = log((float(binTP)/float(binFP)) /
+                                 priorprob) / log(2)
                 else:
                     binlls = 20.0
             else:
                 binlls = -5.0
-            
+
             # save result
-            benchdata[count] = {"cumLLS":lls, "TP":TP, "FP":FP, "MeanBinStatistics":np.mean(binstats), "GeneCoverage":len(already), "BinLLS":binlls}
-            #print("%d\t%.3f\t%d\t%d\t%d\t%f"%(count,lls,TP,FP,len(already),binlls))
+            benchdata[count] = {"cumLLS": lls, "TP": TP, "FP": FP, "MeanBinStatistics": np.mean(
+                binstats), "GeneCoverage": len(already), "BinLLS": binlls}
+            # print("%d\t%.3f\t%d\t%d\t%d\t%f"%(count,lls,TP,FP,len(already),binlls))
 
             # initialize
-            binTP=0
-            binFP=0
-            binstats=list()
+            binTP = 0
+            binFP = 0
+            binstats = list()
 
-    if count%binsize != 0:
+    if count % binsize != 0:
         precision = float(TP)/float(TP+FP)
         recall = float(TP)/float(TP+FN)
         oddratio = precision/randomprecision
         lls = log((float(TP)/float(FP)) / priorprob) / log(2)
 
-        if binTP!=0:
-            if binFP!=0:
+        if binTP != 0:
+            if binFP != 0:
                 binlls = log((float(binTP)/float(binFP)) / priorprob) / log(2)
             else:
                 binlls = 20.0
         else:
             binlls = -5.0
-        benchdata[count] = {"cumLLS":lls,"TP":TP,"FP":FP,"MeanBinStatistics":np.mean(binstats),"GeneCoverage":len(already),"BinLLS":binlls}
-        #print("%d\t%.3f\t%d\t%d\t%d\t%f"%(count,lls,TP,FP,len(already),binlls))
+        benchdata[count] = {"cumLLS": lls, "TP": TP, "FP": FP, "MeanBinStatistics": np.mean(
+            binstats), "GeneCoverage": len(already), "BinLLS": binlls}
+        # print("%d\t%.3f\t%d\t%d\t%d\t%f"%(count,lls,TP,FP,len(already),binlls))
     return pd.DataFrame().from_dict(benchdata).T
 
-corrnetpath =  sys.argv[1] #"/home3/junhacha/test/network_test/condLLS_test/sorted_network_file.tsv"
-#genelistfile = sys.argv[2] #"/home3/junhacha/test/network_test/condLLS_test/genelist.txt" 
-gsdatapath = "/home3/junhacha/public_Data/gold_standard_symbol_HNv3" # [name] [des] [gene1] [g2] [g3] ....
-#genelist = list() #this is the genespace critical for conLLS
+
+# "/home3/junhacha/test/network_test/condLLS_test/sorted_network_file.tsv"
+corrnetpath = sys.argv[1]
+# genelistfile = sys.argv[2] #"/home3/junhacha/test/network_test/condLLS_test/genelist.txt"
+# [name] [des] [gene1] [g2] [g3] ....
+gsdatapath = "/home3/junhacha/public_Data/gold_standard_symbol_HNv3"
+# genelist = list() #this is the genespace critical for conLLS
 
 '''
 if (method != 'sort' and method != 'absort'):
@@ -195,9 +207,10 @@ with open(genelistfile,'r') as fp:
 
 print('loading sorted PCCnet (cut) data')
 
-edges_all = read_edgelist(corrnetpath) # row : gene, column : sample, no NA values, tab seperated
+# row : gene, column : sample, no NA values, tab seperated
+edges_all = read_edgelist(corrnetpath)
 
-#either sort PPC absolute or regular
+# either sort PPC absolute or regular
 '''
 print('make CX from single cell data...')
 
@@ -207,13 +220,13 @@ elif method == 'sort':
     edges_all = calc_pearsonr_fast(scoretable, False) 
 '''
 
-#load gs data accounting for input genespace
+# load gs data accounting for input genespace
 print('loading gold standard list filtered through genelist...')
 gspairs = load_gold_standard_pairs(gsdatapath)
-             
-#calcuate benchmark and output
+
+# calcuate benchmark and output
 print('calculate LLS...')
 benchdata = run_benchmark_LLS(edges_all, gspairs)
 
 print('write output...analysis complete')
-benchdata.to_csv(f"{corrnetpath}.binlls", sep ='\t')
+benchdata.to_csv(f"{corrnetpath}.binlls", sep='\t')
