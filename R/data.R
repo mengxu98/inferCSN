cfde36c5fbefa3ee18d369fb94afd306c0539a1e
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-10-07 10:57:17 +0800

    Update data information

--- R/data.R
+++ R/data.R
@@ -1,31 +1,15 @@
 #' @docType data
 #' @name exampleMatrix
 #'
-#' @title Test data
-#' @description A dataset
-#' diamonds.
-#' @details Success!!
+#' @title Example matrix data
+#' @description The matrix used for reconstruct gene regulatory network.
 #'
-#' @format A matrix with 5000 rows and 18 variables:
-#' \describe{
-#'   \item{matrix}{The matrix used for reconstruct gene regulatory network}
-#'   ...
-#' }
-#' @source \url{NULL}
 NULL
 
 #' @docType data
 #' @name exampleGroundTruth
 #'
-#' @title Test data
-#' @description A dataset
-#' diamonds.
-#' @details Success!!
+#' @title Example ground truth data
+#' @description The data used for calculate the evaluating indicator.
 #'
-#' @format A matrix with 5000 rows and 18 variables:
-#' \describe{
-#'   \item{matrix}{The matrix used for reconstruct gene regulatory network}
-#'   ...
-#' }
-#' @source \url{NULL}
 NULL
