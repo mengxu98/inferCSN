a3a74e2cb9dffa254b4577a73add28f94b2289d4
Meng Xu <86040828+mengxu98@users.noreply.github.com>
2024-01-10 11:16:09 +0800

    Update cran_release.R

--- dev/cran_release.R
+++ dev/cran_release.R
@@ -10,45 +10,11 @@ devtools::run_examples()
 # Check package as CRAN
 rcmdcheck::rcmdcheck(args = c("--no-manual", "--as-cran"))
 
-# Check content
-if (!require("checkhelper", quietly = TRUE)) {
-  install.packages("checkhelper", repos = 'https://thinkr-open.r-universe.dev')
-}
-checkhelper::find_missing_tags()
-
-# Check spelling
-if (!require("spelling", quietly = TRUE)) {
-  install.packages("spelling")
-}
-spelling::spell_check_test(vignettes = TRUE,
-                           error = FALSE,
-                           skip_on_cran = TRUE)
-spelling::spell_check_package()
-
-# check on other distributions
-# _win devel
-devtools::check_win_devel()
-# _rhub
-# devtools::check_rhub()
-rhub::check_on_windows(check_args = "--force-multiarch")
-rhub::check_on_debian(check_args = "--force-multiarch")
-rhub::check_on_fedora(check_args = "--force-multiarch")
-# rhub::check_on_solaris()
-
-# Check reverse dependencies
-usethis::use_git_ignore("revdep/")
-usethis::use_build_ignore("revdep/")
-
-devtools::revdep()
-
 # Update NEWS
 # Bump version manually and add list of changes
 
 # Add comments for CRAN
 usethis::use_cran_comments(open = rlang::is_interactive())
-
-# Upgrade version number
-usethis::use_version(which = c("patch", "minor", "major", "dev")[1])
 
 # Verify you're ready for release, and release
 devtools::release()
