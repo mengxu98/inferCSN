3f1fced44e781070c1eb9164fb43ec7e228a9eba
Meng Xu <86040828+mengxu98@users.noreply.github.com>
2025-10-15 16:34:29 +0800

    remove files

--- scripts/package_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# You can also install the development version use git clone to
-# automatically handle some issues related to dependencies:
-git clone https://github.com/mengxu98/inferCSN.git
-cd inferCSN
-sh scripts/requirements.sh
-R CMD INSTALL . --library=/your/lib/path
